71780f62da7329987216cea84e3ccd3b88984ce3
Oleg <oleg@oleg.pritula>
2017-10-18 22:33:51 +0300

    Update with remarks

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -15,15 +15,18 @@ end
 def sum_to_n? arr, n
   # YOUR CODE HERE Run associated tests via:  `$ rspec spec/part1_spec.rb:42`
   if  arr.length >= 2
-    index = 0 
-    while arr[index] < arr.index(arr.last)
-      elem1 = n-arr[index]
-      if arr.include?(elem1)
-        return true
-        break
-      else
-        index += 1
+    i=0
+    while i<arr.length-1 do
+      j=i+1
+      a = arr[i]
+      while j<arr.length do
+        b=arr[j]
+        if  a+b == n
+          return true
+        end
+        j=j+1
       end
+      i=i+1
     end
   end  
   return false  
@@ -38,48 +41,31 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE Run associated tests via:  `$ rspec -e '#starts_with_consonant?' spec/part2_spec.rb`
- (/\A[^AEIOUaeiou\s\d]/=~s)? true:false
+ (/\A[^AEIOUaeiou\W]/=~s)? true:false
 end
 
 def binary_multiple_of_4? s
-=begin
-Определите метод `binary_multiple_of_4? (S)`, который берет строку и возвращает значение true, если строка представляет двоичное число, которое кратно 4. ПРИМЕЧАНИЕ: убедитесь, что он возвращает false, если строка не является действительным двоичным числом!
-=end
-  # YOUR CODE HERE Run associated tests via:  `$ rspec -e '#binary_multiple_of_4?' spec/part2_spec.rb` You can check your progress on the all the above by running `$ rspec spec/part2_spec.rb`.
-  result = s[/(^[01]{1,})/]
+  # YOUR CODE HERE Run associated tests via:  `$ rspec -e '#binary_multiple_of_4?' spec/part2_spec.rb`
+
+  result = s[/(^[01]+)/]
     if result == nil
       return false
     end
-    a = s
-    b = (a >> 2).to_s(2)
-    if  (b << 2) == a
-      return true
-    end
-    return false
+
+   if result.to_i(2)%4 == 0
+     return true
+   end
+
+
+  return false
+
   end
 
 # Part 3
 
 class BookInStock
-=begin
-Определите класс `BookInStock`, который представляет книгу с ISBN
-номер, `isbn` и цена книги как число с плавающей запятой,
-`цена`, как атрибуты. Запуск связанных тестов с помощью: `$ rspec -e 'getters и seters' spec / part3_spec.rb`
 
-Конструктор должен принять номер ISBN
-(строка, поскольку в реальной жизни номера ISBN могут начинаться с нуля и могут
-включают дефисы) в качестве первого аргумента и цены в качестве второго аргумента и
-должен поднять `ArgumentError` (один из встроенных типов исключений Ruby), если
-номер ISBN - это пустая строка или если цена меньше или равна
-равным нулю. Включите правильные геттеры и сеттеры для этих
-атрибутов. Запуск связанных тестов с помощью: `$ rspec -e 'constructor' spec / part3_spec.rb`
-
-Включите метод `price_as_string`, который возвращает цену
-книга, отформатированная с помощью знака доллара и двух знаков после запятой, то есть цены
-из 20 следует отформатировать как «20,00 долларов США», а цена 33,8 должна отформатировать как
-«$ 33,80»
-=end
-# YOUR CODE HERE
+# YOUR CODE HERE Run associated tests via:  `$ rspec -e 'getters and setters' spec/part3_spec.rb` 
 
   def initialize (isbn, price)
     if isbn.length == 0
@@ -96,9 +82,8 @@ class BookInStock
   
   attr_accessor :isbn, :price #Setters&Getters
 
-  def price_as_string (price)
+  def price_as_string
     "$%.2f" % price
   end
   
 end
-
